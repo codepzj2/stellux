3fb701739da8477c4a4072797c10ca648ae962da
codepzj <email@codepzj.cn>
2025-02-21 15:31:37 +0800

    feat: 优化themeStore逻辑

--- admin/src/store/theme.ts
+++ admin/src/store/theme.ts
@@ -1,24 +1,31 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useThemeStore = defineStore("theme", () => {
-  const tailwindTheme = ref("light");
+export const useThemeStore = defineStore(
+  "theme",
+  () => {
+    const tailwindTheme = ref("light");
 
-  const toggleTheme = () => {
-    tailwindTheme.value = tailwindTheme.value === "light" ? "dark" : "light";
-    document.documentElement.classList.toggle("dark", tailwindTheme.value === "dark");
-  };
+    const toggleTheme = () => {
+      tailwindTheme.value = tailwindTheme.value === "light" ? "dark" : "light";
+      document.documentElement.classList.toggle(
+        "dark",
+        tailwindTheme.value === "dark"
+      );
+    };
 
-  const initTheme = () => {
-    const savedTheme = JSON.parse(localStorage.getItem("theme") || '{"tailwindTheme":"light"}');
-    tailwindTheme.value = savedTheme.tailwindTheme;
-    localStorage.setItem("theme", JSON.stringify({ tailwindTheme: tailwindTheme.value }));
-    document.documentElement.classList.toggle("dark", savedTheme.tailwindTheme === "dark");
-  };
+    const initTheme = () => {
+      document.documentElement.classList.toggle(
+        "dark",
+        tailwindTheme.value === "light"
+      );
+    };
 
-  return { tailwindTheme, toggleTheme, initTheme };
-}, {
-  persist: true,
-});
+    return { tailwindTheme, toggleTheme, initTheme };
+  },
+  {
+    persist: true,
+  }
+);
 
 export default useThemeStore;
